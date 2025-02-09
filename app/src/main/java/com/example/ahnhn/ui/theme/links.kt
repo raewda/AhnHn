ddbaea4b7ecbab21d0e00def1f415e7f3f5ed294
wda <vorobieva2005dasha@gmail.com>
2025-02-09 15:34:52 +0700

    add objects coffee in open class

--- app/src/main/java/com/example/ahnhn/ui/theme/links.kt
+++ app/src/main/java/com/example/ahnhn/ui/theme/links.kt
@@ -5,3 +5,6 @@ package com.example.ahnhn.ui.theme
 //https://tasty.co/article/michelleno/coffee-drinks-recipes - coffee recipe
 
 //https://any.ge/horoscope/api/?sign=aquarius&type=daily&day=today&lang=en - API free
+
+
+//https://www.figma.com/design/2CX3T7pk1V53KGPfJJmEMS/AhHn?node-id=0-1&p=f&t=BDAYMacZPfHaJstF-0 - design icon
